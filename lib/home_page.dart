04bc954aee765b66b55bd5b9b800f2b7745f1600
Ming Ki Chong <unexpected.exception@gmail.com>
2019-02-06 22:39:35 -0800

    refactored address text view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:vibrate/vibrate.dart';
 
 import 'address_button_manager.dart';
 import 'address_store.dart';
+import 'address_text_view.dart';
 import 'constant.dart';
 import 'control_buttons.dart';
 import 'control_button_bloc.dart';
@@ -66,54 +65,10 @@ class BodyStructure extends StatelessWidget {
         SizedBox(height: Constant.SIZED_BOX_HEIGHT),
         ControlButtons(),
         SizedBox(height: Constant.SIZED_BOX_HEIGHT / 2),
-        AddressText(),
+        AddressTextView(),
         SizedBox(height: Constant.SIZED_BOX_HEIGHT),
         MapView(),
       ],
-    );
-  }
-}
-
-class AddressText extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Flexible(
-      child: SingleChildScrollView(
-        child: GestureDetector(
-          onLongPress: () {
-            AddressStore.readAddress().then((value) {
-              Clipboard.setData(ClipboardData(text: value));
-              Scaffold.of(context).showSnackBar(SnackBar(
-                duration: Duration(seconds: 2),
-                content: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.content_copy,
-                      color: Colors.lime,
-                    ),
-                    SizedBox(width: Constant.SIZED_BOX_WIDTH),
-                    Text(
-                      'Address copied to clipboard',
-                      style: TextStyle(fontFamily: 'century_gothic'),
-                    ),
-                  ],
-                ),
-              ));
-              Vibrate.feedback(FeedbackType.selection);
-            });
-          },
-          child: StreamBuilder<String>(
-            initialData: Constant.DEFAULT_ADDRESS,
-            stream: AddressButtonManager.of(context).bloc.addressStream,
-            builder: (context, snapshot) => Text(
-                  snapshot.data,
-                  style: TextStyle(fontSize: Constant.TEXT_ADDRESS_FONT_SIZE),
-                  textAlign: TextAlign.center,
-                ),
-          ),
-        ),
-      ),
     );
   }
 }
